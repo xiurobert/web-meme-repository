392254e24ca45eb94a91a8788830c550086c6b8b
Robert Xiu <xiurobert@gmail.com>
2019-08-14 17:32:48 +0800

    add boilerplate

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,16 @@ router.get('/login', function(req, res, next){
     res.render('login', { title: 'Login'});
 });
 
+router.post('/login', function(req, res, next){
+    // TODO logic when you post to login
+});
+
 router.get('/signup', function(req, res, next) {
     res.render("signup", { title: 'Sign Up'});
+});
+
+router.post('/signup', function(req, res, next) {
+    // TODO logic when you post to signup
 });
 
 module.exports = router;
