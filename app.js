8d264da1a9c2ee576e23f8e36389e29ba37a8f4e
Robert Xiu <xiurobert@gmail.com>
2019-08-14 14:15:42 +0800

    Update gitignore Wtf the i in index isn't getting centered

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+app.disable('view cache');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
