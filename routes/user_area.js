41283837acb41f695ec2d36733b3cb0838c4acab
Robert Xiu <xiurobert@gmail.com>
2019-08-16 10:29:06 +0800

    Looks like you can login with email now too yay

--- routes/user_area.js
+++ routes/user_area.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var user = require("../models/user");
 var auth_mid = require("../mw/requires_login");
 
 router.get('/dash', auth_mid.auth_check, function(req, res, next) {
@@ -8,6 +9,15 @@ router.get('/dash', auth_mid.auth_check, function(req, res, next) {
 
 router.get('/submit', auth_mid.auth_check, function(req, res, next) {
     res.render('user_zone/submit');
+});
+
+router.get('/my_profile', auth_mid.auth_check, function(req, res, next) {
+    var u = user.findById(req.session.userId);
+    res.render('user_zone/my_profile',
+        {
+            "email": u.email,
+            "username": u.username
+        })
 });
 
 
