1c1a961ad1b16763fd329d24bbf2345bbe716e8b
Robert Xiu <xiurobert@gmail.com>
2019-08-16 10:16:08 +0800

    Updated user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
     username: {
         type: String,
         unique: true,
--- routes/auth.js
+++ routes/auth.js
@@ -14,9 +14,9 @@ router.post('/login', function(req, res, next){
     if (req.body.username && req.body.password) {
         User.authenticate(req.body.username, req.body.password, function (error, user) {
             if (error || !user) {
-                var err = new Error('Wrong username or password.');
+                var err = new Error('Wrong username/email or password.');
                 err.status = 401;
-                return res.send("Email or password wrong");
+                return res.send("Wrong username/email or password wrong");
             } else {
                 req.session.userId = user._id;
                 return res.send("Authenticated")
