d170ce5b9dc436aab9f8a67fbce942a87d4dc67e
Robert Xiu <xiurobert@gmail.com>
2019-08-16 10:10:26 +0800

    Made signup properly check for email and not attempt to sign up with blank usernames

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,36 +1,50 @@
 function execSignup() {
+    var email = $("#email").val();
     var username = $("#username").val();
     var pw = $("#password").val();
     var confirmPw = $("#confirmPassword").val();
 
-    if (!username || !pw || !confirmPw) {
+    if (!email || !username || !pw || !confirmPw) {
         $("#inputsBlank").modal();
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        $("#emailError").modal();
+        return;
     }
 
     if (pw !== confirmPw) {
         $("#somethingNoMatch").modal();
-    } else {
-        $.ajax({
-            url: "/auth/signup",
-            method: "PUT",
-            data: {
-                username: username,
-                password: pw,
-                confirmPassword: confirmPw
-            },
-            success: function(result) {
-                if (result.includes("already exists")) {
-                    $("#duplicateUser").modal();
-                } else if (result.includes("match")) {
-                    $("#somethingNoMatch").modal();
-                } else {
-                    $(".alert").removeAttr("hidden");
-                    window.location.href = "/z/dash";
-                }
-            }
-        })
+        return;
     }
 
+    $.ajax({
+        url: "/auth/signup",
+        method: "PUT",
+        data: {
+            email: email,
+            username: username,
+            password: pw,
+            confirmPassword: confirmPw
+        },
+        success: function(result) {
+            if (result.includes("already exists")) {
+                $("#duplicateUser").modal();
+            } else if (result.includes("match")) {
+                $("#somethingNoMatch").modal();
+            } else {
+                $(".alert").removeAttr("hidden");
+                window.location.href = "/z/dash";
+            }
+        }
+    })
+
+}
+// Definitely not copied
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function runOnEnter(e) {
