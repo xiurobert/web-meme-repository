d6d52bc7596878a5751e5d0fff41411a706aa84c
Robert Xiu <xiurobert@gmail.com>
2019-08-16 15:04:01 +0800

    Holy shit I finally finished the tag input library

--- public/javascripts/tag_input.js
+++ public/javascripts/tag_input.js
@@ -1,6 +1,9 @@
 var dtag_tags = [];
+
+var sel = $(".tag-input");
+
 $(document).ready(function() {
-    var sel = $(".tag-input");
+
 
     var c2a = sel.parent();
     if (sel.parent().hasClass("input-group")){
@@ -9,29 +12,60 @@ $(document).ready(function() {
 
     c2a.append('<div class="dtag-input-tags"></div>');
 
+    function deleteLastTag() {
+        dtag_tags.pop();
+        $(".dtag-input-tags").children().last().remove();
+    }
+
     sel.on('keyup',function(e) {
         if (e.which === 13 || e.which === 32 || e.which === 188) {
             e.preventDefault();
             var val = sel.val();
+            val = val.trim();
             sel.val('');
             if (val.includes(",")) {
                 val = val.replace(",", "")
             }
 
+            if (val === "") {
+                return;
+            }
+
             if (dtag_tags.includes(val)) {
                 return;
             }
-            $(".dtag-input-tags").append('<span class="dtag-input-tag">' + val + '<span class="dtag-deltag">&times;</span></span>');
+
+            // Disallow spaces in tags to prevent element from being chopped into half
+            if (val.includes(" ")) {
+                return;
+            }
+            $(".dtag-input-tags").append(
+                '<span class="dtag-input-tag">' +
+                    '<span class="dtag-val">' + val + '</span>' +
+                    '<span class="dtag-deltag">&nbsp; &times;</span>' +
+                '</span>');
 
             dtag_tags.push(val);
         }
 
         if (e.which === 8) {
-            dtag_tags.pop();
-            $(".dtag-input-tags").children().last().remove();
+            if ($(e.target).val() === "") {
+                e.preventDefault();
+                deleteLastTag();
+            }
+
         }
 
     });
+
+    $(document).on('click',".dtag-deltag", function(event) {
+        var valToPop = $(event.target).parent().find($(".dtag-val")).html();
+        if (dtag_tags.indexOf(valToPop) !== -1) dtag_tags.splice(dtag_tags.indexOf(valToPop), 1);
+        $(event.target).parent().remove();
+    });
+
+
+
 
 });
 
