340827a7062655f25eed584ef733b765833299e5
Robert Xiu <xiurobert@gmail.com>
2019-08-20 14:32:21 +0800

    fixed noResults msg showing up

--- public/javascripts/searchResults.js
+++ public/javascripts/searchResults.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("footer").hide();
     $("#noResultsMsg").hide();
     if (!$(".meme-result").length) {
+        $("#noResultsMsg").removeAttr("hidden");
         $("#noResultsMsg").show();
     }
 });
