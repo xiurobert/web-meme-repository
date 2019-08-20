21660cb1fe5fdfe4ac6535f404a69da75fb95fb8
Robert Xiu <xiurobert@gmail.com>
2019-08-20 12:53:03 +0800

    also linked up search button for index page

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,3 +1,8 @@
+$(document).on('keypress', function(e) {
+    if (e.which === 13 && $("input[type=search]").val()) {
+        searchMeme();
+    }
+});
 
 function searchMeme() {
     window.location.href = "/meme/search?q=" + encodeURIComponent($("input[type=search]").val())
