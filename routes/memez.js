ca19b7c62effb6cfbe95e6c19a187c8786280013
Robert Xiu <xiurobert@gmail.com>
2019-08-21 15:17:54 +0800

    (Possibly) implement meme editing and changed all DB queries to promise-based queries

--- routes/memez.js
+++ routes/memez.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var Meme = require("../models/memes");
-var user = require("../models/user");
-var auth_mid = require("../mw/requires_login");
+let express = require('express');
+let router = express.Router();
+
+let sanitizer = require("sanitizer");
+
+let Meme = require("../models/memes");
+let user = require("../models/user");
+let auth_mid = require("../mw/requires_login");
 
 router.get('/:id', function(req, res, next) {
     if (!req.params.id) {
@@ -45,31 +48,33 @@ router.get('/:memeId/delete', auth_mid.auth_check, function(req, res, next) {
     if (!req.params.memeId) {
         return res.status(400).end("Meme id is blank")
     }
-    Meme.findOne({
-        key: req.params.memeId
-    }, function(err, meme) {
-        if (err) {
-            return res.status(500).end("DB error");
-        }
+    Meme.findOne({key: req.params.memeId})
+        .then(
+            function(meme) {
+                if (!meme) {
+                    return res.status(404).end("That meme doesn't exist!");
+                }
 
-        if (!meme) {
-            return res.status(404).end("That meme doesn't exist!");
-        }
+                if (meme.uId !== req.session.userId) {
+                    return res.status(403).end("As much as you hate the meme (or the user that submitted it) " +
+                        "you can't delete what is not yours")
+                }
 
-        if (meme.uId !== req.session.userId) {
-            return res.status(403).end("As much as you hate the meme (or the user that submitted it) " +
-                "you can't delete what is not yours")
-        }
+                Meme.deleteOne({key: req.params.memeId})
+                    .then(function() {
+                        return res.status(200).end("Meme deleted successfully");
+                    })
+                    .catch(function() {
+                        return res.status(500).end("DB error could not delete meme");
+                    })
 
-        Meme.deleteOne({key: req.params.memeId}, function(err) {
-            if (err) {
-                return res.status(500).end("DB error could not delete meme");
             }
-
-            return res.status(200).end("Meme deleted successfully");
-        })
-
-    })
+        )
+        .catch(
+            function() {
+                return res.status(500).end("DB error");
+            }
+        )
 });
 
 router.get('/:memeId/update', auth_mid.auth_check, function(req, res, next) {
@@ -77,24 +82,41 @@ router.get('/:memeId/update', auth_mid.auth_check, function(req, res, next) {
     if (!req.params.memeId) {
         return res.status(400).end("Meme id is blank")
     }
-    Meme.findOne({
-        key: req.params.memeId
-    }, function(err, meme) {
-        if (err) {
-            return res.status(500).end("DB error");
-        }
-
-        if (!meme) {
-            return res.status(404).end("That meme doesn't exist!");
-        }
-
-        if (meme.uId !== req.session.userId) {
-            return res.status(403).end("As much as you hate this meme or its user you can't edit what is not yours")
-        }
 
 
+    Meme.findOne({key: req.params.memeId})
+        .lean()
+        .then(function(meme) {
 
-    })
+
+            if (!meme) {
+                return res.status(404).end("That meme doesn't exist!");
+            }
+
+            if (meme.uId !== req.session.userId) {
+                return res.status(403).end("As much as you hate this meme or its user you can't edit what is not yours")
+            }
+
+            meme.title = sanitizer.sanitize(req.body.newTitle);
+            function cleanTags() {
+                let cleanedArray;
+                for (tag in req.body.newTags) {
+                    cleanedArray.push(sanitizer.sanitize(tag));
+                }
+                return cleanedArray;
+            }
+
+            meme.keywords = cleanTags();
+            meme.description = sanitizer.sanitize(req.body.newDesc);
+
+            meme.save()
+                .then(function() {
+                    return res.status(200).end("Successfully updated meme");
+                });
+        }).catch(function() {
+        return res.status(500).end("DB error");
+
+    });
 
 });
 
@@ -104,7 +126,7 @@ router.get("/search", function(req, res, next) {
         return next();
     }
 
-    let memeQuery = Meme.find(
+    Meme.find(
         {
             $or: [
                 {$text: {$search: new RegExp("/" + req.query.q + "/")}},
@@ -114,17 +136,16 @@ router.get("/search", function(req, res, next) {
         }
     )
         .limit(25)
-        .lean();
-
-    memeQuery.then(function(docs) {
-        let queryExecTime = docs.pop().execTime;
-        res.render("meme/searchEngine",
-            {
-                memes: docs,
-                logged_in: (req.session && req.session.userId),
-                searchTime: queryExecTime
-            })
-    })
+        .lean()
+        .then(function(docs) {
+            let queryExecTime = docs.pop().execTime;
+            res.render("meme/searchEngine",
+                {
+                    memes: docs,
+                    logged_in: (req.session && req.session.userId),
+                    searchTime: queryExecTime
+                })
+        })
         .catch(function(err) {
             res.status = 500;
             return next(err);
